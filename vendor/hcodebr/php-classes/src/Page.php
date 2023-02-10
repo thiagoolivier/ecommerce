3100d2436c72dc5128e54ba8ba166d7062b481c5
thiago oliveira <thiagoolivier53@gmail.com>
2023-02-10 09:59:23 -0400

    Created functions for user login with validation.

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -11,6 +11,8 @@ class Page
     private $options = [];
 
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data" => []
     ];
 
@@ -31,7 +33,7 @@ class Page
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) $this->tpl->draw("header");
 
     }
 
@@ -51,7 +53,7 @@ class Page
 
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
     }
 }
