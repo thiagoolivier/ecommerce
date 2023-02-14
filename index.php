5288fdf38f2a16d47b6a7dadca6bad39fd5d1c78
thiago oliveira <thiagoolivier53@gmail.com>
2023-02-14 13:33:22 -0400

    Started to configure the password recovery part of the application.

--- index.php
+++ index.php
@@ -87,6 +87,15 @@ $app->get("/admin/users/create", function() {
 $app->get("/admin/users/:iduser/delete", function($iduser) {
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
 	
 });
 
@@ -131,7 +140,51 @@ $app->post("/admin/users/create", function() {
 $app->post("/admin/users/:iduser", function($iduser) {
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
 	
+});
+
+$app->get("/admin/forgot", function() {
+	
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl('forgot');
+
+});
+
+$app->post("/admin/forgot", function() {
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/admin/forgot/sent", function(){
+	
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl('forgot-sent');
+
 });
 
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -8,6 +8,11 @@ use \Hcode\Model;
 class User extends Model
 {
     const SESSION = "User";
+	const SECRET = "HcodePhp7_Secret";
+	const SECRET_IV = "HcodePhp7_Secret_IV";
+	const ERROR = "UserError";
+	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSucesss";
 
     public static function login($login, $password)
     {
@@ -67,6 +72,7 @@ class User extends Model
         ));
 
         $this->setData($results[0]);
+
     }
 
     public static function logout()
@@ -91,15 +97,113 @@ class User extends Model
         $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
             
             ":desperson" => $this->getdesperson(),
-            ":deslogin" =>$this->getdeslogin(),
-            ":despassword" =>$this->getdespassword(),
-            ":desemail" =>$this->getdesemail(),
-            ":nrphone" =>$this->getnrphone(),
-            ":inadmin" =>$this->getinadmin(),
+            ":deslogin" => $this->getdeslogin(),
+            ":despassword" => $this->getdespassword(),
+            ":desemail" => $this->getdesemail(),
+            ":nrphone" => $this->getnrphone(),
+            ":inadmin" => $this->getinadmin(),
 
         ));
 
         $this->setData($results[0]);
 
     }
+
+    public function update()
+    {
+
+        $sql = new Sql();
+        
+        $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":iduser" => $this->getiduser(),
+            ":desperson" => $this->getdesperson(),
+            ":deslogin" => $this->getdeslogin(),
+            ":despassword" => $this->getdespassword(),
+            ":desemail" => $this->getdesemail(),
+            ":nrphone" => $this->getnrphone(),
+            ":inadmin" => $this->getinadmin(),
+
+        ));
+
+        $this->setData($results[0]);
+
+
+    }
+
+    public function delete()
+    {
+
+        $sql = new Sql();
+
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser" => $this->getiduser()
+        ));
+    }
+
+    public static function getForgot($email, $inadmin = true)
+    {
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+            SELECT *
+            FROM tb_persons a
+            INNER JOIN tb_users b USING(idperson)
+            WHERE a.desemail = :email
+        ", array(
+            ":email" => $email
+        ));
+
+        if (count($results) === 0)
+        {
+            
+            throw new \Exception("Não foi possível recuperar a senha.");
+
+        } 
+        else 
+        {
+
+            $data = $results[0];
+
+            $results2 = $sql->select("CALL sp_userpasswordsrecoveries_create(:iduser, :desip)", array(
+                ":iduser"=>$data["iduser"],
+                ":desip"=>$_SERVER["REMOTE_ADDR"]
+            ));
+
+            if (count($results2) === 0)
+            {
+                throw new \Exception("Não foi possível recuperar a senha.");
+            }
+            else
+            {
+                $dataRecovery = $results2[0];
+
+                $code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				if ($inadmin === true) {
+
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+				} else {
+
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+					
+				}				
+
+				$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store", "forgot", array(
+					"name"=>$data['desperson'],
+					"link"=>$link
+				));				
+
+				$mailer->send();
+
+				return $link;
+
+            }
+
+        }
+    }
+
 }
