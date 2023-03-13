bf8d412de20d2f6d473b0346b69bb2f2fc5d3bd2
thiago oliveira <thiagoolivier53@gmail.com>
2023-03-13 13:05:17 -0400

    Freight calculation system.

--- site.php
+++ site.php
@@ -69,7 +69,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", [
 		'cart' => $cart->getValues(),
-		'products' => $cart->getProducts()
+		'products' => $cart->getProducts(),
+		'error' => Cart::getMsgError()
 	]);
 	
 });
@@ -128,5 +129,16 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	exit;
 
 });
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
+
+})
 
 ?>
